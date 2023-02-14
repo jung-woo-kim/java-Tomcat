8afbfe418ed2c9119fa9a90ec6de39ab19376ac0
JungWoo <rlawjddn103@naver.com>
2023-02-14 20:13:13 +0900

    docs(HttpRequestStartLineTest): HttpRequestStartLineTest 테스트 추가

---
+++ src/test/java/util/HttpRequestStartLineTest.java
@@ -0,0 +1,27 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpRequestStartLineTest {
+
+    @Test
+    void 정상_동작() {
+        HttpRequestStartLine startLine = HttpRequestStartLine.from("GET ./index.html HTTP1.1");
+
+        assertEquals("GET",startLine.getHttpMethod().getMethod());
+        assertEquals("./index.html",startLine.getPath());
+        assertEquals("HTTP1.1",startLine.getVersion());
+    }
+
+    @Test
+    void 지원하지_않는_메소드() {
+        assertThrows(IllegalArgumentException.class,()->HttpRequestStartLine.from("PATCH ./index.html HTTP1.1"));
+    }
+
+    @Test
+    void 이상한_문장() {
+        assertThrows(IllegalArgumentException.class,()->HttpRequestStartLine.from("안녕하세요"));
+    }
+}
