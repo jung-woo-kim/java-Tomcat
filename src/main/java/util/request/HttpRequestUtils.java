cdbf556bfb8834631f565e1525559a9557c9dc69
JungWoo <rlawjddn103@naver.com>
2023-02-14 20:14:59 +0900

    refactor(HttpRequestStartLine): HttpRequestStartLine 패키지 변경

---
+++ src/main/java/util/request/HttpRequestUtils.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.util.List;
+
+public class HttpRequestUtils {
+//    public static Header parseHeader(String headerLine) {
+//        String[] firstLine = split(headerLine, " ");
+//        return new Header(firstLine[0], firstLine[1]);
+//    }
+
+    private static String[] split(String values,String separator) {
+        return values.split(separator);
+    }
+}
