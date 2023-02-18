e26b799e2be093537ccf510aa64e950256417d5c
JungWoo <rlawjddn103@naver.com>
2023-02-18 18:29:43 +0900

    feat(HttpHeader): 리다이렉트를 위한 Location 헤더 추가

--- src/main/java/util/HttpHeader.java
+++ src/main/java/util/HttpHeader.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum HttpHeader {
-    CONTENT_LENGTH("Content-Length"), CONTENT_TYPE("Content-Type");
+    CONTENT_LENGTH("Content-Length"), CONTENT_TYPE("Content-Type"), LOCATION("Location");
 
     private String header;
 
