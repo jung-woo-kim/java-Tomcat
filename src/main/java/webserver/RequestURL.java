6a83258415062348f8bc990c7f9fe2ae4178aeec
JungWoo <rlawjddn103@naver.com>
2023-02-18 18:32:50 +0900

    feat(RequestURL): 요청 URL 패스 상수화

--- src/main/java/webserver/RequestURL.java
+++ src/main/java/webserver/RequestURL.java
@@ -1,7 +1,7 @@
 package webserver;
 
 public enum RequestURL {
-    SIGNUP("/user/create"),DEFAULT("./webapp"),INDEX("/index.html");
+    SIGNUP("/user/create"), ROOT("./webapp"),INDEX("/index.html");
 
     private String url;
 
